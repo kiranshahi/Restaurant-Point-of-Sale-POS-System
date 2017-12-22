8190a9c1a3deb8b130a9cd44768a735a32861ebd
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-22 23:27:52 +0545

    ResturentPOS form has been added.

--- Restaurant POS System/Program.cs
+++ Restaurant POS System/Program.cs
@@ -16,7 +16,7 @@ namespace Restaurant_POS_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ResturentPOS());
         }
     }
 }
--- Restaurant POS System/ResturentPOS.cs
+++ Restaurant POS System/ResturentPOS.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Restaurant_POS_System
 {
-    public partial class Form1 : Form
+    public partial class ResturentPOS : Form
     {
-        public Form1()
+        public ResturentPOS()
         {
             InitializeComponent();
         }
