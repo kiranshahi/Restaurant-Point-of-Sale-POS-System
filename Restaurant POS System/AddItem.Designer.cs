5e1895f47f0d1ae9922a60bb7ba104a31ba20674
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-24 10:14:43 +0545

    close current form event added.

--- Restaurant POS System/AddItem.Designer.cs
+++ Restaurant POS System/AddItem.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddItem));
             this.lblItemPrice = new System.Windows.Forms.Label();
             this.lblItemName = new System.Windows.Forms.Label();
             this.txtItemPrice = new System.Windows.Forms.TextBox();
             this.txtItemName = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblItemPrice
@@ -42,7 +44,7 @@
             this.lblItemPrice.Location = new System.Drawing.Point(27, 115);
             this.lblItemPrice.Name = "lblItemPrice";
             this.lblItemPrice.Size = new System.Drawing.Size(53, 24);
-            this.lblItemPrice.TabIndex = 9;
+            this.lblItemPrice.TabIndex = 0;
             this.lblItemPrice.Text = "Price";
             // 
             // lblItemName
@@ -52,7 +54,7 @@
             this.lblItemName.Location = new System.Drawing.Point(27, 27);
             this.lblItemName.Name = "lblItemName";
             this.lblItemName.Size = new System.Drawing.Size(101, 24);
-            this.lblItemName.TabIndex = 8;
+            this.lblItemName.TabIndex = 0;
             this.lblItemName.Text = "Item Name";
             // 
             // txtItemPrice
@@ -61,7 +63,7 @@
             this.txtItemPrice.Location = new System.Drawing.Point(31, 154);
             this.txtItemPrice.Name = "txtItemPrice";
             this.txtItemPrice.Size = new System.Drawing.Size(464, 29);
-            this.txtItemPrice.TabIndex = 7;
+            this.txtItemPrice.TabIndex = 2;
             // 
             // txtItemName
             // 
@@ -69,7 +71,7 @@
             this.txtItemName.Location = new System.Drawing.Point(31, 66);
             this.txtItemName.Name = "txtItemName";
             this.txtItemName.Size = new System.Drawing.Size(464, 29);
-            this.txtItemName.TabIndex = 6;
+            this.txtItemName.TabIndex = 1;
             // 
             // btnSave
             // 
@@ -81,16 +83,32 @@
             this.btnSave.Location = new System.Drawing.Point(390, 241);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(105, 28);
-            this.btnSave.TabIndex = 10;
+            this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(545, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(34, 26);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // AddItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(581, 292);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.lblItemPrice);
             this.Controls.Add(this.lblItemName);
@@ -98,7 +116,7 @@
             this.Controls.Add(this.txtItemName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddItem";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddItem";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -112,5 +130,6 @@
         private System.Windows.Forms.TextBox txtItemPrice;
         private System.Windows.Forms.TextBox txtItemName;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- Restaurant POS System/AddItem.cs
+++ Restaurant POS System/AddItem.cs
@@ -24,16 +24,23 @@ namespace Restaurant_POS_System
 
             if (decimal.TryParse(txtItemPrice.Text, out price) && !String.IsNullOrWhiteSpace(itemName))
             {
-                //int count = menuGrid.Rows.Count - 1;
-                //menuGrid.Rows.Add();
-                //menuGrid.Rows[count].Cells[0].Value = count + 1;
-                //menuGrid.Rows[count].Cells[1].Value = itemName;
-                //menuGrid.Rows[count].Cells[2].Value = price;
+                Item newItem = new Item();
+                newItem.ItemName = itemName;
+                newItem.ItemPrice = price;
+
+                ResturentPOS mainFormObj = new ResturentPOS();
+                mainFormObj.addItemToGrid(newItem);
+                this.Close();
             }
             else
             {
                 MessageBox.Show("You have entered invalid input.", "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
